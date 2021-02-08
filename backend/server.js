9d1be14934cbbb21ae115f64722bbd94ed0ec187
BigDogandHisGirlfriendNelly <47969657+GodzillaJim@users.noreply.github.com>
2021-02-08 23:46:22 +0300

    Build path

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(router);
 if (process.env.NODE_ENV === 'PRODUCTION') {
   app.use(express.static(path.join(__dirname, '../build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
   );
 }
 app.listen(process.env.PORT || 5000, (error) => {
